4595a6cbd1f2227b906c7f743349a697e14469e0
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2020-06-01 10:09:20 +0000

    lab 4.3senza esercizi aggiuntivi

--- RelabClient/src/app/app.component.ts
+++ RelabClient/src/app/app.component.ts
@@ -26,12 +26,13 @@ export class AppComponent implements OnInit {
   circleLng: number = 0;
   maxRadius: number = 400; //Voglio evitare raggi troppo grossi
   radius : number = this.maxRadius;
+  serverUrl : string = "https://3000-fc2c0052-a226-4a47-9a24-f2bb17f5c2ae.ws-eu01.gitpod.io";
   constructor(public http: HttpClient) {
   }
 
-  prepareData = (data: GeoFeatureCollection) => {
+   prepareData = (data: GeoFeatureCollection) => {
     this.geoJsonObject = data
-    console.log(this.geoJsonObject)
+    console.log(this.geoJsonObject);
   }
 
     //Metodo che riceve i dati e li aggiunge ai marker
@@ -55,14 +56,14 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://3000-b000e620-b380-41e7-bdf7-308686199961.ws-eu01.gitpod.io/");
-    this.obsGeoData.subscribe(this.prepareData);
+    //this.obsGeoData = this.http.get<GeoFeatureCollection>("https://3000-fc2c0052-a226-4a47-9a24-f2bb17f5c2ae.ws-eu01.gitpod.io/");
+    //this.obsGeoData.subscribe(this.prepareData);
     //Effettua la chiamatata al server per ottenere l’elenco dei vettori energetici
   }
   cambiaFoglio(foglio) : boolean
   {
     let val = foglio.value; //Commenta qui: creiamo variabile val che assume il valore del foglio specificato nella label
-    this.obsCiVett = this.http.get<Ci_vettore[]>(`https://3000-b000e620-b380-41e7-bdf7-308686199961.ws-eu01.gitpod.io/ci_vettore/${val}`); //Commenta qui: facciamo una richiesta http.get al server
+    this.obsCiVett = this.http.get<Ci_vettore[]>(`${this.serverUrl}/ci_vettore/${val}`); //Commenta qui: facciamo una richiesta http.get al server
     this.obsCiVett.subscribe(this.prepareCiVettData); //Commenta qui: qui ci "sottoscriviamo e quando arrivano i dati viene lanciato il metodo prepareCiVettData"
     console.log(val);
     return false;
@@ -84,36 +85,81 @@ export class AppComponent implements OnInit {
   {
     console.log(circleCenter); //Voglio ottenere solo i valori entro questo cerchio
     console.log(this.radius);
-
-    this.circleLat = circleCenter.coords.lat; //Aggiorno le coordinate del cerchio
-    this.circleLng = circleCenter.coords.lng; //Aggiorno le coordinate del cerchio
-
-    //Non conosco ancora le prestazioni del DB, non voglio fare ricerche troppo onerose
+    this.circleLat = circleCenter.coords.lat;
+    this.circleLng = circleCenter.coords.lng;
     if(this.radius > this.maxRadius)
     {
       console.log("area selezionata troppo vasta sarà reimpostata a maxRadius");
-       this.radius = this.maxRadius;
+      this.radius = this.maxRadius;
     }
-    console.log ("raggio in gradi " + (this.radius * 0.00001)/1.1132)
+
+    let raggioInGradi = (this.radius * 0.00001)/1.1132;
+
+
+    const urlciVett = `${this.serverUrl}/ci_geovettore/
+    ${this.circleLat}/
+    ${this.circleLng}/
+    ${raggioInGradi}`;
+
+    const urlGeoGeom = `${this.serverUrl}/geogeom/
+    ${this.circleLat}/
+    ${this.circleLng}/
+    ${raggioInGradi}`;
+    //Posso riusare lo stesso observable e lo stesso metodo di gestione del metodo cambiaFoglio
+    //poichè riceverò lo stesso tipo di dati
+    //Divido l'url andando a capo per questioni di leggibilità non perchè sia necessario
+    this.obsCiVett = this.http.get<Ci_vettore[]>(urlciVett);
+    this.obsCiVett.subscribe(this.prepareCiVettData);
+
+    this.obsGeoData = this.http.get<GeoFeatureCollection>(urlGeoGeom);
+    this.obsGeoData.subscribe(this.prepareData);
+
+    //console.log ("raggio in gradi " + (this.radius * 0.00001)/1.1132)
 
     //Voglio spedire al server una richiesta che mi ritorni tutte le abitazioni all'interno del cerchio
 
-  let raggioInGradi = (this.radius * 0.00001)/1.1132;
-  //Posso riusare lo stesso observable e lo stesso metodo di gestione del metodo
-  //cambiaFoglio poichè riceverò lo stesso tipo di dati
-  //Divido l'url andando a capo per questioni di leggibilità non perchè sia necessario
-    this.obsCiVett = this.http.get<Ci_vettore[]>(`http://https://3000-b000e620-b380-41e7-bdf7-308686199961.ws-eu01.gitpod.io//ci_geovettore/
-    ${this.circleLat}/
-    ${this.circleLng}/
-    ${raggioInGradi}`);
-    this.obsCiVett.subscribe(this.prepareCiVettData);
   }
+
   styleFunc = (feature) => {
+    console.log(feature);
     return ({
       clickable: false,
-      fillColor: this.fillColor,
-      strokeWeight: 1
+      fillColor: this.avgColorMap(feature.j.media),
+      strokeWeight: 1,
+      fillOpacity : 1  //Fill opacity 1 = opaco (i numeri tra 0 e 1 sono le gradazioni di trasparenza)
     });
+  }
+    //Mappa rosso-verde
+avgColorMap = (media) =>
+  {
+    if(media <= 36) return "#00FF00";
+    if(36 < media && media <= 40) return "#33ff00";
+    if(40 < media && media <= 58) return "#66ff00";
+    if(58 < media && media <= 70) return "#99ff00";
+    if(70 < media && media <= 84) return "#ccff00";
+    if(84 < media && media <= 100) return "#FFFF00";
+    if(100 < media && media <= 116) return "#FFCC00";
+    if(116 < media && media <= 1032) return "#ff9900";
+    if(1032 < media && media <= 1068) return "#ff6600";
+    if(1068 < media && media <= 1948) return "#FF3300";
+    if(1948 < media && media <= 3780) return "#FF0000";
+    return "#FF0000"
+  }
+  //mappa scala di verdi
+  avgColorMapGreen = (media) =>
+  {
+    if(media <= 36) return "#EBECDF";
+    if(36 < media && media <= 40) return "#DADFC9";
+    if(40 < media && media <= 58) return "#C5D2B4";
+    if(58 < media && media <= 70) return "#ADC49F";
+    if(75 < media && media <= 84) return "#93B68B";
+    if(84 < media && media <= 100) return "#77A876";
+    if(100 < media && media <= 116) return "#629A6C";
+    if(116 < media && media <= 1032) return "#558869";
+    if(1032 < media && media <= 1068) return "#487563";
+    if(1068 < media && media <= 1948) return "#3B625B";
+    if(1948 < media && media <= 3780) return "#2F4E4F";
+    return "#003000" //Quasi nero
   }
 
 }
--- RelabClient/src/app/models/marker.model.ts
+++ RelabClient/src/app/models/marker.model.ts
@@ -1,64 +1,67 @@
 import { Icon } from './icon.model';
 
 export class Marker {
-    icon = {}
-   //Quando creo un nuovo marker e verifico quale label viene passata al costruttore, se contiene il testo
-   //“Gas naturale” o “Energia elettrica” (abbreviati in Gas e Elettrica) imposto l’icona e cancello
-   //l’etichetta
-    constructor(public lat: number, public lng: number, public label?: string)
-    {
-        if (this.label.includes("Gas")) {
-          this.icon = new Icon ( './assets/img/gas.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("elettrica")) {
-          this.icon = new Icon ( './assets/img/electricity.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("solide")) {
-          this.icon = new Icon ( './assets/img/tree-50-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("olio")) {
-          this.icon = new Icon ( './assets/img/water-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("Teleriscaldamento")) {
-          this.icon = new Icon ( './assets/img/power-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("RSU")) {
-          this.icon = new Icon ( './assets/img/trash-9-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("liquide")) {
-          this.icon = new Icon ( './assets/img/toxic-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("NULL")) {
-          this.icon = new Icon ( './assets/img/question-mark-6-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("GPL")) {
-          this.icon = new Icon ( './assets/img/gas-pump-2-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-        if (this.label.includes("gasolio")) {
-          this.icon = new Icon ( './assets/img/warning-22-32.ico', 24 ); //da modificare per ogni icona
-          this.label = "";
-          return;
-        }
-
-        this.icon =new Icon ( './assets/img/question-mark-6-32.ico', 24 );
-
+  icon = {}
+  //Quando creo un nuovo marker e verifico quale label viene passata al costruttore, se contiene il testo
+  //“Gas naturale” o “Energia elettrica” (abbreviati in Gas e Elettrica) imposto l’icona e cancello
+  //l’etichetta
+  constructor(public lat: number, public lng: number, public label?: string) {
+    if (this.label ==null || this.label == "undefined" || this.label == "null"){
+      this.icon = new Icon('./assets/img/question-mark-6-32.ico', 24);
+      this.label = "";
+      return;
     }
+    if (this.label.includes("Gas")) {
+      this.icon = new Icon('./assets/img/gas.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("elettrica")) {
+      this.icon = new Icon('./assets/img/electricity.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("solide")) {
+      this.icon = new Icon('./assets/img/tree-50-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("olio")) {
+      this.icon = new Icon('./assets/img/water-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("Teleriscaldamento")) {
+      this.icon = new Icon('./assets/img/power-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("RSU")) {
+      this.icon = new Icon('./assets/img/trash-9-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("liquide")) {
+      this.icon = new Icon('./assets/img/toxic-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("NULL")) {
+      this.icon = new Icon('./assets/img/question-mark-6-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("GPL")) {
+      this.icon = new Icon('./assets/img/gas-pump-2-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+    if (this.label.includes("gasolio")) {
+      this.icon = new Icon('./assets/img/warning-22-32.ico', 24); //da modificare per ogni icona
+      this.label = "";
+      return;
+    }
+
+
+  }
 }
